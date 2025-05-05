bfacf6f70db2f475791b6dae240342695e267867
Simphoria258 <olegjansoul@gmail.com>
2025-05-06 00:50:43 +0300

    Add files via upload

---
+++ deepseek_bash_20250505_436ebe (1).sh
@@ -0,0 +1,6 @@
+git clone https://github.com/ваш-логин/AIROHPMYS-Translator.git
+cd AIROHPMYS-Translator
+# Скопируйте ваши файлы в эту папку
+git add .
+git commit -m "Додано перекладач AIROHPMYS"
+git push
